332b53b7eddb9eaf43305775d5804732daf95f8e
Munish Gowtham Baskaran <munishgowthambaskaran@Munishs-MacBook-Air.local>
2024-01-17 17:31:12 +0530

    implementation of new redux action is complete

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
+import * as courseActions from '../../redux/actions/courseActions'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
 class CoursesPage extends React.Component {
     state = {
         course: {
@@ -14,7 +17,11 @@ class CoursesPage extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        alert(this.state.course.title)
+        /**
+         * dispatch allows us to dispatch our actions, it is injected automatically if we don't add mapDispatchToProps to connect
+         * we are dispatching create course action and passing it to the course
+         */
+        this.props.actions.createCourse(this.state.course)
     }
 
     render() {
@@ -28,8 +35,41 @@ class CoursesPage extends React.Component {
                     value={this.state.course.title}
                 />
                 <input type="submit" value="Save" />
+                {this.props.courses.map((course) => (
+                    <div key={course.id}>{course.title}</div>
+                ))}
             </form>
         )
     }
 }
-export default CoursesPage
+
+function mapStateToProps(state) {
+    /**
+     * Request only the data your component needs.. otherwise if we expose the entire redux store then the component
+     * will re-render when any data changes in redux store
+     */
+    return { courses: state.courses }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        /**
+         * here we can pass all of our course actions or just one course to bind  bindActionCreators(courseActions.createCourse, dispatch
+         */
+
+        actions: bindActionCreators(courseActions, dispatch),
+    }
+}
+
+CoursesPage.propTypes = {
+    actions: PropTypes.object.isRequired,
+    courses: PropTypes.array.isRequired,
+}
+
+/** connects our react container components to redux
+ * it takes in two parameters mapStateToProps and mapDispatchToProps (optional)
+ * mapDispatchToProps determines which actions we want to expose on our components.
+ * if we omit mapDispatchToProps then our component gets dispatch property injected automatically via props
+ */
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,8 @@
+import * as types from './../actions/actionTypes'
+
 export default function courseReducer(state = [], action) {
-    debugger
     switch (action.type) {
-        case 'CREATE_COURSE':
+        case types.CREATE_COURSE:
             /* returns a new copy of all existing courses with the new course we passed in using spread operator */
             return [...state, { ...action.course }]
         default:
