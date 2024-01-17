2e09507eb662b45dcadb9f89b8f589d75b563ddc
Munish Gowtham Baskaran <munishgowthambaskaran@Munishs-MacBook-Air.local>
2024-01-17 15:55:05 +0530

    redux configuration complete

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 export default function courseReducer(state = [], action) {
+    debugger
     switch (action.type) {
-        case 'CREATE_ACTION':
+        case 'CREATE_COURSE':
             /* returns a new copy of all existing courses with the new course we passed in using spread operator */
             return [...state, { ...action.course }]
         default:
